e426d3ede2eaa029e5e4d190c56aaf4339f979a6
Beatriz Tsumoto <beatriz.tsumoto@aluno.senai.br>
2025-09-22 08:56:50 -0300

    first commit

---
+++ src/Models/dados.js
@@ -0,0 +1,113 @@
+const series =
+[
+    {
+      "id": 1,
+      "titulo": "Stranger Things",
+      "genero": "Ficção Científica, Terror",
+      "canal": "Netflix",
+      "temporadas": 4,
+      "episodios": 34,
+      "anoInicio": 2016,
+      "anoFim": "Ativo",
+      "avaliacao": 8.7
+    },
+    {
+      "id": 2,
+      "titulo": "The Office (U.S.)",
+      "genero": "Comédia",
+      "canal": "NBC",
+      "temporadas": 9,
+      "episodios": 201,
+      "anoInicio": 2005,
+      "anoFim": 2013,
+      "avaliacao": 9.0
+    },
+    {
+      "id": 3,
+      "titulo": "Game of Thrones",
+      "genero": "Fantasia, Drama",
+      "canal": "HBO",
+      "temporadas": 8,
+      "episodios": 73,
+      "anoInicio": 2011,
+      "anoFim": 2019,
+      "avaliacao": 9.2
+    },
+    {
+      "id": 4,
+      "titulo": "Breaking Bad",
+      "genero": "Drama, Suspense",
+      "canal": "AMC",
+      "temporadas": 5,
+      "episodios": 62,
+      "anoInicio": 2008,
+      "anoFim": 2013,
+      "avaliacao": 9.5
+    },
+    {
+      "id": 5,
+      "titulo": "How I Met Your Mother",
+      "genero": "Comédia, Romance",
+      "canal": "CBS",
+      "temporadas": 9,
+      "episodios": 208,
+      "anoInicio": 2005,
+      "anoFim": 2014,
+      "avaliacao": 8.3
+    },
+    {
+      "id": 6,
+      "titulo": "The Mandalorian",
+      "genero": "Ficção Científica, Ação",
+      "canal": "Disney+",
+      "temporadas": 3,
+      "episodios": 24,
+      "anoInicio": 2019,
+      "anoFim": "Ativo",
+      "avaliacao": 8.7
+    },
+    {
+      "id": 7,
+      "titulo": "Black Mirror",
+      "genero": "Ficção Científica, Antologia",
+      "canal": "Channel 4, Netflix",
+      "temporadas": 6,
+      "episodios": 27,
+      "anoInicio": 2011,
+      "anoFim": "Ativo",
+      "avaliacao": 8.8
+    },
+    {
+      "id": 8,
+      "titulo": "Peaky Blinders",
+      "genero": "Drama, Crime",
+      "canal": "BBC Two",
+      "temporadas": 6,
+      "episodios": 36,
+      "anoInicio": 2013,
+      "anoFim": 2022,
+      "avaliacao": 8.8
+    },
+    {
+      "id": 9,
+      "titulo": "The Witcher",
+      "genero": "Fantasia, Ação",
+      "canal": "Netflix",
+      "temporadas": 3,
+      "episodios": 24,
+      "anoInicio": 2019,
+      "anoFim": "Ativo",
+      "avaliacao": 8.0
+    },
+    {
+      "id": 10,
+      "titulo": "The Crown",
+      "genero": "Drama Histórico",
+      "canal": "Netflix",
+      "temporadas": 5,
+      "episodios": 50,
+      "anoInicio": 2016,
+      "anoFim": "Ativo",
+      "avaliacao": 8.6
+    }
+  ]
